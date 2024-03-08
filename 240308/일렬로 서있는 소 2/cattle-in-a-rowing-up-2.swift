2441dc9b74ca58788fabe028e8d2873c62233bed
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-08 22:07:12 +0900

    Title: 일렬로 서있는 소 2, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240308/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.swift
+++ 240308/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.swift
@@ -3,12 +3,8 @@ let cows = readLine()!.split { $0 == " " }.map { Int(String($0))! }
 
 var result = 0
 for i in 0..<n {
-    for j in i..<n {
-        for k in j..<n {
-            guard i != j && j != k && i != k else {
-                continue
-            }
-            
+    for j in i+1..<n {
+        for k in j+1..<n {
             if cows[i] <= cows[j] && cows[j] <= cows[k] {
                 result += 1
             }
