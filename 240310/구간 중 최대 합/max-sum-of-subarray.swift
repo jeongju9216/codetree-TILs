c5b809a07d6f65dbbd6e2a7dedf80207bbf4280e
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-10 17:20:10 +0900

    Title: 구간 중 최대 합, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240310/구간 중 최대 합/max-sum-of-subarray.swift
+++ 240310/구간 중 최대 합/max-sum-of-subarray.swift
@@ -3,7 +3,7 @@ let n = nk[0], k = nk[1]
 let arr = readLine()!.split { $0 == " " }.map { Int(String($0))! }
 
 var result = Int.min
-for i in 0..<n-k {
+for i in 0...n-k {
     let sum = arr[i..<i+k].reduce(0, +)
     result = max(result, sum)
 }
