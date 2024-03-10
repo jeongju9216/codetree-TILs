43bcbc283657b5354028dfeabc5ee5b5deb3ba1f
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-10 17:24:07 +0900

    Title: 아름다운 수열 2, Time: 18ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240310/아름다운 수열 2/beautiful-sequence-2.swift
@@ -0,0 +1,15 @@
+let nm = readLine()!.split { $0 == " " }.map { Int(String($0))! }
+let n = nm[0], m = nm[1]
+let arrA = readLine()!.split { $0 == " " }.map { Int(String($0))! }
+let arrB = readLine()!.split { $0 == " " }.map { Int(String($0))! }
+
+var result = 0
+let setB = Set(arrB)
+for i in 0...n-m {
+    let setA = Set(arrA[i..<i+m])
+    if setA == setB {
+        result += 1
+    }
+}
+
+print(result)
