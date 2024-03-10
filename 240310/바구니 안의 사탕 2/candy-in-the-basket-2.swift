c82ba077eafb0151437a935a8deead5d84e6b8e1
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-10 19:42:57 +0900

    Title: 바구니 안의 사탕 2, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240310/바구니 안의 사탕 2/candy-in-the-basket-2.swift
@@ -0,0 +1,23 @@
+let maxCount = 100
+let nk = readLine()!.split { $0 == " " }.map { Int(String($0))! }
+let n = nk[0], k = nk[1]
+
+var arr: [Int] = Array(repeating: 0, count: maxCount + 1)
+for _ in 0..<n {
+    let input = readLine()!.split { $0 == " " }.map { Int(String($0))! }
+    let candies = input[0]
+    let point = input[1]
+    arr[point] += candies
+}
+
+var result = Int.min
+for i in 0..<maxCount {
+    guard i - k >= 0, i + k <= maxCount else {
+        continue
+    }
+
+    let sum = arr[i-k...i+k].reduce(0, +)
+    result = max(result, sum)
+}
+
+print(result)
