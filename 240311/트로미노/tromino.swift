b28f749c34bd37cc8a20f2025114bb2d940d9978
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-11 19:50:50 +0900

    Title: 트로미노, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240311/트로미노/tromino.swift
@@ -0,0 +1,53 @@
+let dx = [
+    [0, 1, 1],
+    [0, 0, 1],
+    [0, 1, 1],
+    [0, 0, 1],
+    [0, 0, 0],
+    [0, 1, 2]
+]
+let dy = [
+    [0, 0, 1],
+    [0, 1, 0],
+    [0, 0, -1],
+    [0, 0, 1],
+    [0, 1, 2],
+    [0, 0, 0]
+]
+
+let nm = readLine()!.split { $0 == " " }.map { Int(String($0))! }
+let n = nm[0], m = nm[1]
+
+var arr: [[Int]] = []
+for _ in 0..<n {
+    let input = readLine()!.split { $0 == " " }.map { Int(String($0))! }
+    arr.append(input)
+}
+
+var result = Int.min
+for i in 0..<n {
+    for j in 0..<n {        
+        for k in 0..<dx.count {
+            var isValid = true
+            var sum = 0
+
+            for t in 0..<dx[k].count {
+                let nx = i + dx[k][t]
+                let ny = j + dy[k][t]
+
+                guard 0..<n ~= nx, 0..<m ~= ny else {
+                    isValid = false
+                    break
+                }
+
+                sum += arr[nx][ny]
+            }
+
+            if isValid {
+                result = max(result, sum)
+            }
+        }
+    }
+}
+
+print(result)
