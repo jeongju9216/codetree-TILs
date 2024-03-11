38b779a19468d7f6abebfef860b6626ad494094f
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-11 19:18:57 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240311/행복한 수열의 개수/number-of-happy-sequence.swift
@@ -0,0 +1,39 @@
+func isHappy(_ arr: [Int]) -> Bool {
+    var count = 1
+    for i in 1..<n {
+        if arr[i] == arr[i-1] {
+            count += 1
+        } else {
+            count = 1
+        }
+
+        if count == m {
+            return true
+        }
+    }
+
+    return false
+}
+
+let nm = readLine()!.split { $0 == " " }.map { Int(String($0))! }
+let n = nm[0], m = nm[1]
+var arr: [[Int]] = []
+for _ in 0..<n {
+    let input = readLine()!.split { $0 == " " }.map { Int(String($0))! }
+    arr.append(input)
+}
+
+var result = 0
+for i in 0..<n {
+    let hLine = arr[i]
+    if isHappy(hLine) {
+        result += 1
+    }
+
+    let vLine = (0..<n).map { arr[$0][i] }
+    if isHappy(vLine) {
+        result += 1
+    }
+}
+
+print(result)
