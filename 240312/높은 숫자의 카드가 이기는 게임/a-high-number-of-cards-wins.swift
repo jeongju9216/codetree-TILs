55ab9553c6e0af328965dcf7fa11edc7697736da
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-12 23:13:00 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 599ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

--- 240312/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.swift
+++ 240312/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.swift
@@ -1,19 +1,26 @@
 let n = Int(readLine()!)!
-var setA: Set<Int> = Set(1...2*n)
-var setB: Set<Int> = []
+var arrB: [Int] = []
 for _ in 0..<n {
     let input = Int(readLine()!)!
-    setB.insert(input)
-    setA.remove(input)
+    arrB.append(input)
 }
 
+var arrA: [Int] = []
+for i in 1...2*n {
+    if !arrB.contains(i) {
+        arrA.append(i)
+    }
+}
+
+arrA.sort()
+arrB.sort()
+
 var result = 0
-for numB in setB {
-    if let higherA = setA.filter { $0 > numB }.min() {
-        setA.remove(higherA)
+var bIndex = 0
+for i in 0..<n {
+    if bIndex < n && arrA[i] > arrB[bIndex] {
         result += 1
-    } else {
-        setA.remove(setA.min()!)
+        bIndex += 1
     }
 }
 
