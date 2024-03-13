c6ffc07cf5de47d8273d031edbf820e58c13e338
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-13 22:56:29 +0900

    Title: 중앙값 계산 2, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240313/중앙값 계산 2/get-median-2.swift
@@ -0,0 +1,16 @@
+let n = Int(readLine()!)!
+let arr = readLine()!.split { $0 == " " }.map { Int(String($0))! }
+
+var numbers: [Int] = []
+var result: [Int] = []
+for i in 0..<arr.count {
+    numbers.append(arr[i])
+
+    if i % 2 == 0 {
+        numbers.sort(by: <)
+        let midIndex = numbers.count / 2
+        result.append(numbers[midIndex])
+    }
+}
+
+print(result)
