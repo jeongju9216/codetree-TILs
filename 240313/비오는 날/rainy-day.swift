759d036a8023bffce7b18a4c30c0db5f683bc088
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-13 22:30:11 +0900

    Title: 비오는 날, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240313/비오는 날/rainy-day.swift
+++ 240313/비오는 날/rainy-day.swift
@@ -4,11 +4,27 @@ struct WeatherInfo {
     let weather: String
 }
 
+var days: [String: Int] = [
+    "Mon": 0,
+    "Tue": 1,
+    "Wed": 2,
+    "Thu": 3,
+    "Fri": 4,
+    "Sat": 5,
+    "Sun": 6
+]
+
 let n = Int(readLine()!)!
 var arr: [WeatherInfo] = []
 for _ in 0..<n {
     let input = readLine()!.split { $0 == " " }.map { String($0) }
     arr.append(WeatherInfo(date: input[0], day: input[1], weather: input[2]))
+}
+
+arr.sort { 
+    $0.date == $1.date 
+    ? days[$0.day]! < days[$1.day]! 
+    : $0.date < $1.date 
 }
 
 let firstRain = arr.first(where: { $0.weather == "Rain" })!
