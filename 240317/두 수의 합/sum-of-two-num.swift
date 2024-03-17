d9fab15c80b06ab7dd11895071a575c8ba36751d
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-17 18:09:33 +0900

    Title: 두 수의 합, Time: 108ms, Memory: 6MB, Status: Passed - CodeTree

--- 240317/두 수의 합/sum-of-two-num.swift
+++ 240317/두 수의 합/sum-of-two-num.swift
@@ -2,18 +2,11 @@ let nm = readLine()!.split { $0 == " " }.map { Int(String($0))! }
 let n = nm[0], m = nm[1]
 let arr = readLine()!.split { $0 == " " }.map { Int(String($0))! }
 
-var dict: [Int: [Int]] = [:]
-for i in 0..<arr.count {
-    dict[arr[i], default: []].append(i)
-}
-
+var count: [Int: Int] = [:]
 var result = 0
 for num in arr {
-    let num1 = dict[num] ?? []
-    let num2 = dict[m - num] ?? []
-    result += num1.count * num2.count
-
-    dict[num] = nil
-    dict[m - num] = nil
+    let diff = m - num
+    result += count[diff] ?? 0
+    count[num, default: 0] += 1
 }
 print(result)
