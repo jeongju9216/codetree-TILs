a20b1cbbb993841544f3500e8b8fce6afbc91d58
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-17 14:55:57 +0900

    Title: 수열 조작, Time: 1016ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

--- 240317/수열 조작/sequence-manipulation.swift
+++ 240317/수열 조작/sequence-manipulation.swift
@@ -2,11 +2,9 @@ let n = Int(readLine()!)!
 var arr = Array(1...n)
 
 while arr.count > 1 {
-    var tmp: [Int] = arr.reversed()
-    tmp.removeLast()
-    let lastItem = tmp.removeLast()
-    arr = tmp.reversed()
-    arr.append(lastItem)
+    arr.removeFirst()
+    arr.append(arr[0])
+    arr.removeFirst()
 }
 
 print(arr[0])
