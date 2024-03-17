232a3bd47043d5f590fc05fb3baaf349d8d69b09
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-17 18:27:44 +0900

    Title: 세 수의 합, Time: 40ms, Memory: 0MB, Status: Passed - CodeTree

--- 240317/세 수의 합/sum-of-three-num.swift
+++ 240317/세 수의 합/sum-of-three-num.swift
@@ -2,17 +2,21 @@ let nk = readLine()!.split { $0 == " " }.map { Int(String($0))! }
 let n = nk[0], k = nk[1]
 let arr = readLine()!.split { $0 == " " }.map { Int(String($0))! }
 
+var counts: [Int: Int] = [:]
+for num in arr {
+    counts[num, default: 0] += 1
+}
+
 var result = 0
 for i in 0..<n {
-    var counts: [Int: Int] = [:]
+    counts[arr[i], default: 0] -= 1
 
-    for j in 0..<n where i != j {
+    for j in 0..<i {
         let num1 = arr[i]
         let num2 = arr[j]
-        let diff = k - (num1 + num2)
+        let diff = k - num1 - num2
         
         result += counts[diff] ?? 0
-        counts[diff, default: 0] += 1
     }
 }
 print(result)
