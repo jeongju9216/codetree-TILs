648af029f72ea8b1b54de30303244041fe4946f8
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-17 12:12:27 +0900

    Title: 독서실의 거리두기 3, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240317/독서실의 거리두기 3/study-cafe-keeping-distance-3.swift
@@ -0,0 +1,36 @@
+let n = Int(readLine()!)!
+var arr = readLine()!.map { Int(String($0))! }
+
+var maxDistance = Int.min
+var prevPoint = 0
+var left = 0, right = 0
+for i in 1..<n {
+    guard arr[i] == 1 else {
+        continue
+    }
+
+    let dist = i - prevPoint
+    if maxDistance < dist {
+        maxDistance = dist
+        left = prevPoint
+        right = i
+    }
+    prevPoint = i
+}
+
+let center = (right - left) / 2
+arr[center] = 1
+
+var result = Int.max
+prevPoint = 0
+for i in 1..<n {
+    guard arr[i] == 1 else {
+        continue
+    }
+
+    let dist = i - prevPoint
+    result = min(result, dist)
+    prevPoint = i
+}
+
+print(result)
