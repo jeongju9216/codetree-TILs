6e2c0ab22f722922a59b71aec5705fa2391eb64c
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-17 18:20:19 +0900

    Title: 세 수의 합, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240317/세 수의 합/sum-of-three-num.swift
@@ -0,0 +1,18 @@
+let nk = readLine()!.split { $0 == " " }.map { Int(String($0))! }
+let n = nk[0], k = nk[1]
+let arr = readLine()!.split { $0 == " " }.map { Int(String($0))! }
+
+var result = 0
+for i in 0..<n {
+    var counts: [Int: Int] = [:]
+
+    for j in 0..<n where i != j {
+        let num1 = arr[i]
+        let num2 = arr[j]
+        let diff = num1 + num2 + k
+        
+        result += counts[diff] ?? 0
+        counts[diff, default: 0] += 1
+    }
+}
+print(result)
