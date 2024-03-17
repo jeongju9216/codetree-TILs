c1694da3f2f89b1884190666ed73bf5ed9841ff0
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-17 18:06:33 +0900

    Title: 두 수의 합, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240317/두 수의 합/sum-of-two-num.swift
+++ 240317/두 수의 합/sum-of-two-num.swift
@@ -2,19 +2,18 @@ let nm = readLine()!.split { $0 == " " }.map { Int(String($0))! }
 let n = nm[0], m = nm[1]
 let arr = readLine()!.split { $0 == " " }.map { Int(String($0))! }
 
-var set: Set<Int> = []
-for num in arr {
-    set.insert(num)
+var dict: [Int: [Int]] = [:]
+for i in 0..<arr.count {
+    dict[arr[i], default: []].append(i)
 }
 
 var result = 0
-var visited: Set<Int> = []
 for num in arr {
-    if set.contains(m - num) && !visited.contains(num) {
-        visited.insert(num)
-        visited.insert(m - num)
-        result += 1
-    }
-}
+    let num1 = dict[num] ?? []
+    let num2 = dict[m - num] ?? []
+    result += num1.count * num2.count
 
+    dict[num] = nil
+    dict[m - num] = nil
+}
 print(result)
