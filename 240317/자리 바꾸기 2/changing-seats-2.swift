4e2c1cf7118f026c3091687b66ffdc946ca19d4a
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-17 21:51:14 +0900

    Title: 자리 바꾸기 2, Time: 1087ms, Memory: 17MB, Status: Time Limit Exceed - CodeTree

--- 240317/자리 바꾸기 2/changing-seats-2.swift
+++ 240317/자리 바꾸기 2/changing-seats-2.swift
@@ -1,33 +1,32 @@
 let nk = readLine()!.split { $0 == " " }.map { Int(String($0))! }
 let n = nk[0], k = nk[1]
-var points: [[Int]] = []
+var points: [(a: Int, b: Int)] = []
 for _ in 0..<k {
     let input = readLine()!.split { $0 == " " }.map { Int(String($0))! }
-    points.append(input)
+    points.append((input[0] - 1, input[1] - 1))
 }
 
-var sets: [Set<Int>] = Array(repeating: [], count: n + 1)
-var arr = Array(0...n)
+var sets: [Set<Int>] = Array(repeating: [], count: n)
+var arr = Array(0..<n)
 for num in arr {
     sets[num].insert(num)
 }
 
 for _ in 0..<3 {
     for i in 0..<k {
-        let a = points[i][0]
-        let b = points[i][1]
+        let a = points[i].a
+        let b = points[i].b
         
         let tmp = arr[a]
         arr[a] = arr[b]
         arr[b] = tmp
 
-        for j in 1...n {
-            sets[j].insert(arr[j])
-        }
+        sets[a].insert(arr[a])
+        sets[b].insert(arr[b])
     }
 }
 
-for i in 1...n {
+for i in 0..<n {
     var count = 0
     for set in sets {
         if set.contains(i) {
