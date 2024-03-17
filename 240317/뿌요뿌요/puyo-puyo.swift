0d30fe817961407112a701d6ee5ba557ef8d1868
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-17 22:11:50 +0900

    Title: 뿌요뿌요, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240317/뿌요뿌요/puyo-puyo.swift
@@ -0,0 +1,47 @@
+let dx = [-1, 1, 0, 0]
+let dy = [0 ,0, -1, 1]
+
+func dfs(_ x: Int, _ y: Int) {
+    let item = board[x][y]
+    count += 1
+    
+    for i in 0..<4 {
+        let nx = x + dx[i]
+        let ny = y + dy[i]
+
+        guard 0..<n ~= nx, 0..<n ~= ny else {
+            continue
+        }
+
+        if board[nx][ny] == item && !visited[nx][ny] {
+            visited[nx][ny] = true
+            dfs(nx, ny)
+        }
+    }
+}
+
+let n = Int(readLine()!)!
+var board: [[Int]] = []
+for _ in 0..<n {
+    let input = readLine()!.split { $0 == " " }.map { Int(String($0))! }
+    board.append(input)
+}
+
+var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: n), count: n)
+var result = 0, blockNum = 0
+var count = 0
+for i in 0..<n {
+    for j in 0..<n {
+        if !visited[i][j] {
+            count = 0
+            visited[i][j] = true
+            dfs(i, j)
+            if result < count {
+                result = count
+                blockNum = board[i][j]
+            }
+        }
+    }
+}
+
+print("\(blockNum) \(result)")
