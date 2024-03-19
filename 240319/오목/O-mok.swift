5149d9ba6ee74be668a74a43fd7f92bba01a73b5
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-19 21:10:44 +0900

    Title: 오목, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240319/오목/O-mok.swift
@@ -0,0 +1,82 @@
+func checkH(_ x: Int, _ y: Int) -> Bool {
+    let minRange = max(y-2, 0)
+    let maxRange = min(y+2, 18)
+    let item = board[x][y]
+    for i in minRange...maxRange {
+        if board[x][i] != item {
+            return false
+        }
+    }
+    return true
+}
+
+func checkV(_ x: Int, _ y: Int) -> Bool {
+    let minRange = max(x-2, 0)
+    let maxRange = min(x+2, 18)
+    let item = board[x][y]
+    for i in minRange...maxRange {
+        if board[i][y] != item {
+            return false
+        }
+    }
+    return true
+}
+
+func checkC1(_ x: Int, _ y: Int) -> Bool {
+    let minXRange = max(x-2, 0)
+    let maxXRange = min(x+2, 18)
+    let minYRange = max(y-2, 0)
+    let maxYRange = min(y+2, 18)
+    let item = board[x][y]
+    for i in minXRange...maxXRange {
+        for j in minYRange...maxYRange {
+            if board[i][j] != item {
+                return false
+            }
+        }
+    }
+    return true
+}
+
+func checkC2(_ x: Int, _ y: Int) -> Bool {
+    let minXRange = max(x-2, 0)
+    let maxXRange = max(x+2, 18)
+    let minYRange = max(y-2, 0)
+    let maxYRange = max(y+2, 18)
+    let item = board[x][y]
+    for i in (minXRange...maxXRange).reversed() {
+        for j in minYRange...maxYRange {
+            if board[i][j] != item {
+                return false
+            }
+        }
+    }
+    return true
+}
+
+var board: [[Int]] = []
+for _ in 0..<19 {
+    let input = readLine()!.split { $0 == " " }.map { Int(String($0))! }
+    board.append(input)
+}
+
+var winColor = 0
+var winMid: [Int] = []
+for i in 0..<19 where winColor == 0 {
+    for j in 0..<19 {
+        guard board[i][j] > 0 else {
+            continue
+        }
+
+        if checkV(i, j) || checkH(i, j) || checkC1(i, j) || checkC2(i, j) {
+            winColor = board[i][j] == 1 ? 1 : 2
+            winMid = [i + 1, j + 1]
+            break
+        }
+    }
+}
+
+print(winColor)
+if winColor > 0 {
+    print("\(winMid[0]) \(winMid[1])")
+}
