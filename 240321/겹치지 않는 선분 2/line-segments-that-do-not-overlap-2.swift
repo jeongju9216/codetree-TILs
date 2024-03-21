208b39cdf94f641a279c925b7b10704930bd56f1
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-21 21:05:53 +0900

    Title: 겹치지 않는 선분 2, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240321/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.swift
+++ 240321/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.swift
@@ -3,13 +3,9 @@ typealias Point = (s: Int, e: Int)
 func check(_ p1: Point, _ p2: Point) -> Bool {
     if p1.s < p2.s && p1.e < p2.e {
         return true
-    } else if p1.s > p2.s && p1.e > p2.e {
-        return true
     }
 
     if p1.e <= p2.s {
-        return true
-    } else if p1.s >= p2.e {
         return true
     }
 
@@ -23,11 +19,15 @@ for _ in 0..<n {
     arr.append((input[0], input[1]))
 }
 
+arr.sort { $0.s < $1.s }
+
 var result = 0
 for i in 0..<n {
     var isChecked = true
     for j in 0..<n where i != j {
-        if !check(arr[i], arr[j]) {
+        let minIndex = min(i, j)
+        let maxIndex = max(i, j)
+        if !check(arr[minIndex], arr[maxIndex]) {
             isChecked = false
             break
         }
