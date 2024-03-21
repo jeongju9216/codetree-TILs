3c24403f978870c5eb6f4cdbd6df9f4df2e2485f
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-21 22:56:40 +0900

    Title: 상해버린 치즈, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240321/상해버린 치즈/rotten-cheese.swift
+++ 240321/상해버린 치즈/rotten-cheese.swift
@@ -18,19 +18,21 @@ for _ in 0..<s {
 // i 치즈를 infoList의 모든 사람이 t 시간 전에 먹었는지 확인한다
 var result = 0
 for i in 1...m {
-    var count = 0
+    var isOkay = true
+    // i 치즈가 상했을 때 모든 기록에 모순이 없는지 확인
     for record in records {
-        let info = infoList.filter { $0.p == record.p }
-        for man in info {
-            if man.m == i && man.t < record.t {
-                count += 1
-                break
-            }
+        // record의 사람이 i 치즈를 t 시간 전에 먹은 정보
+        let info = infoList.filter { $0.p == record.p && $0.m == i && $0.t < record.t }
+        // 해당하는 내용이 없으면 모순
+        if info.isEmpty {
+            isOkay = false
+            break
         }
     }
 
     // 모든 record와 비교했을 때 모순이 없음
-    if count == records.count {
+    if isOkay {
+        // i 치즈는 상한 치즈일 수 있음 -> i 치즈를 먹은 사람 개수 -> 약의 개수
         let ateCheesePerson = infoList.filter { $0.m == i }.count
         result = max(result, ateCheesePerson)
     }
