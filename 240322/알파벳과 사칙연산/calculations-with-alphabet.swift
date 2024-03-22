d4a7dbd32f83ae209cd573360b9f59ea8502ab8b
유정주 JeongJu Yu <dbwjdwn9216@gmail.com>
2024-03-22 20:55:23 +0900

    Title: 알파벳과 사칙연산, Time: 12ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240322/알파벳과 사칙연산/calculations-with-alphabet.swift
@@ -0,0 +1,46 @@
+func item(_ alphabet: String, _ values: [Int]) -> Int {
+    values[dict[alphabet]!]
+}
+
+func cal(_ values: [Int]) -> Int {
+    var result = 0
+    var nums: [Int] = []
+    var op = ""
+    for ch in arr {
+        if dict[ch] == nil {
+            op = ch
+        } else if nums.isEmpty {
+            nums.append(item(ch, values))
+        } else {
+            let num1 = nums[0]
+            let num2 = item(ch, values)
+            if op == "+" {
+                result = (num1 + num2)  
+            } else if op == "-" {
+                result = (num1 - num2)
+            } else if op == "*" {
+                result = (num1 * num2)
+            }
+            nums = [result]
+        }
+    }
+    return result
+}
+
+func backtracking(_ values: [Int]) {
+    guard values.count < dict.keys.count else {
+        result = max(result, cal(values))
+        return
+    }
+
+    for i in 1...4 {
+        backtracking(values + [i])
+    }
+}
+
+var arr = Array(readLine()!).map { String($0) }
+var dict: [String: Int] = ["a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5]
+
+var result = 0
+backtracking([])
+print(result)
